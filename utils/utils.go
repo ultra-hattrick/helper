c530e1aa903ddba1977446603ef580ba251cf4c9
HuGoL <hugomode@gmail.com>
2024-06-27 11:15:45 -0400

    calculo de dias

--- utils/utils.go
+++ utils/utils.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/dghubble/oauth1"
 	"github.com/gin-gonic/gin"
@@ -110,4 +111,47 @@ func GetResultsFromHattrick(pathHattrick string, v any) error {
 	}
 	// helperLog.Logger.Debug().Msgf("--->Arena: %v", hattrickData.Match.DetailsMatch.Arena)
 	return nil
+}
+
+// GetLastDayAndPlusDays returns two dates:
+// 1. Last Friday with the time set to START_TIME_TRAINING_UTC
+// 2. The date of the first date plus 3 days (time.Hour * 72) for example
+func GetLastDayAndPlusDuration(weekDayUTC time.Weekday, plusDuration time.Duration, timeIntUTC string) (time.Time, time.Time, error) {
+	// Get the current time
+	now := time.Now().In(time.UTC)
+
+	// Find the last Friday in UTC
+	helperLog.Logger.Debug().Msgf("now.Weekday(): %d , now: %v", now.Weekday(), now)
+	daysSinceFriday := (int(now.Weekday()) + 7 - int(weekDayUTC)) % 7
+	helperLog.Logger.Debug().Msgf("daysSinceFriday: %v", daysSinceFriday)
+	lastFridayUTC := now.AddDate(0, 0, -daysSinceFriday)
+
+	// Parse START_TIME_TRAINING to get hour, minute, second
+	startTimeParts := strings.Split(timeIntUTC, ":")
+	if len(startTimeParts) != 3 {
+		return time.Time{}, time.Time{}, fmt.Errorf("invalid START_TIME_TRAINING format")
+	}
+	hour, err := strconv.Atoi(startTimeParts[0])
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("invalid hour in START_TIME_TRAINING")
+	}
+	minute, err := strconv.Atoi(startTimeParts[1])
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("invalid minute in START_TIME_TRAINING")
+	}
+	second, err := strconv.Atoi(startTimeParts[2])
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("invalid second in START_TIME_TRAINING")
+	}
+
+	// Set the last Friday date with START_TIME_TRAINING in UTC
+	lastFridayUTC = time.Date(lastFridayUTC.Year(), lastFridayUTC.Month(), lastFridayUTC.Day(), hour, minute, second, 0, time.UTC)
+
+	// Convert last Friday to local time
+	location := time.Now().Location()
+	lastFridayLocal := lastFridayUTC.In(location)
+
+	plusThreeDaysLocal := lastFridayLocal.Add(plusDuration)
+
+	return lastFridayLocal, plusThreeDaysLocal, nil
 }
